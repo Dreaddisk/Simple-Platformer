5232ebff9a819773403d5bd8a55dd2bec452807f
Jose M. Puebla <joey_manuel@outlook.com>
2018-12-10 22:17:07 -0500

    2.Scripts
    a few scripts were made.

---
+++ Simple_Platformer_Prototype/Assets/Scripts/GameSceneManager.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameSceneManager : MonoBehaviour
+{
+    public Camera mainCamera;
+    public Text scoreText;
+    public Text gameOverText;
+    public PlayerController player;
+
+    int score;
+    float gameTimer;
+    bool gameOver;
+
+    private void Start()
+    {
+        Time.timeScale = 1;
+
+        player.OnHitGoomba += OnHitGoomba;
+        player.OnHitspike += OnGameOver;
+        player.OnHitOrv += OnGameWin;
+
+        scoreText.enabled = true;
+        gameOverText.enabled = false;
+    }
+
+    private void Update()
+    {
+        mainCamera.transform.position = new Vector3(Mathf.Lerp(mainCamera.transform.position.x, Time.deltaTime * 10.0f),
+            Mathf.Lerp(mainCamera.transform.position.y, player.transform.position.y, Time.deltaTime * 10.0f),
+            mainCamera.transform.position.z);
+
+        if(gameOver)
+        {
+            if(Input.GetKeyDown("r"))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+
+            return;
+        }
+
+        scoreText.text = "Score: " + score;
+
+        if (player.transform.position.y < -10)
+            OnGameOver();
+
+    }
+
+    void OnHitGoomba()
+    {
+        this.score += 100;
+    }
+
+    void OnGameOver()
+    {
+        gameOver = true;
+
+        scoreText.enabled = false;
+        gameOverText.enabled = true;
+        gameOverText.text = "Game Over!\nPress R to Restart";
+
+        Time.timeScale = 0;
+    }
+
+    void OnGameWin()
+    {
+        gameOver = true;
+
+        scoreText.enabled = false;
+        gameOverText.enabled = true;
+
+        gameOverText.text = "You Win!\n Press R to Restart";
+
+        Time.timeScale = 0;
+    }
+
+
+
+
+} // main class
---
+++ Simple_Platformer_Prototype/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public delegate void OnHitSpikeAction();
+    public delegate void OnHitGoombaAction();
+    public delegate void OnHitOrbAction();
+
+    public OnHitGoombaAction OnHitGoomba;
+    public OnHitSpikeAction OnHitSpike;
+    public OnHitOrbAction OnHitOrb;
+
+    float speed = 1000.0f;
+    float jumpSpeed = 5000.0f;
+
+    Vector3 leftBound;
+    Vector3 rightBound;
+    bool canJump;
+
+    private void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if(Input.GetKeyDown("left") || (Input.GetKeyDown("a")))
+        {
+            this.GetComponent<Rigidbody2D>().AddForce(Vector2.left * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown("right") || (Input.GetKeyDown("d")))
+        {
+            this.GetComponent<Rigidbody2D>().AddForce(Vector2.right * speed * Time.deltaTime);
+        }
+
+        if(Input.GetKeyDown("Space"))
+        {
+            this.GetComponent<Rigidbody2D>().AddForce(Vector2.right * jumpSpeed * Time.deltaTime);
+        }
+    }
+
+    void Jump(bool force = false)
+    {
+        if(canJump || force)
+        {
+            canJump = false;
+
+            this.GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpSpeed * Time.deltaTime);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Bound")
+        {
+            canJump = true;
+        }
+
+        if (collision.gameObject.GetComponent<EnemyController>() != null)
+        {
+            EnemyController enemy = collision.gameObject.GetComponent<EnemyController>();
+
+            if (this.transform.position.y > enemy.transform.position.y + enemy.GetComponent<BoxCollider2D>().size.y / 2)
+            {
+                GameObject.Destroy(collision.gameObject);
+
+                Jump(true);
+
+                if(OnHitGoomba != null)
+                {
+                    OnHitGoomba();
+                }
+            }
+            else
+            {
+                if(OnHitSpike != null)
+                {
+                    OnHitGoomba();
+                }
+            }
+        }
+
+
+    }
+} // main class
